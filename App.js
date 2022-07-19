09baebe279559ee38005cf399458febb327eb2f4
jeppung <jeppung@gmail.com>
2022-07-19 09:04:37 +0700

    Initial Init

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, StatusBar, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
+import Sandbox from './components/Sandbox';
 
-export default function App() {
+const App = () => {
+
+  const [todos, setTodos] = useState([
+    {
+      text: 'buy coffee',
+      key: 1
+    },
+    {
+      text: 'create an app',
+      key: 2
+    },
+    {
+      text: 'play on the switch',
+      key: 3
+    },
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+
+    if(text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      })
+    }else{
+      Alert.alert('OOPS!', 'ToDo must be over 3 chars long', [
+        {
+          text: 'Understood',
+          onPress: () => console.log('alert closed')
+        }
+      ])
+    }
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Sandbox/>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss()
+    }}>
+      <View style={styles.container}>
+        <StatusBar barStyle={'light-content'} hidden/>
+        <Header judul='My Todos'/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={
+                ({item}) =>
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              }
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff'
   },
-});
+  content: {
+    flex: 1,
+    // backgroundColor: 'red',
+    padding: 40
+  },
+  list: {
+    flex: 1,
+    // backgroundColor: 'yellow',
+    marginTop: 20
+  }
+})
+
+export default App;
